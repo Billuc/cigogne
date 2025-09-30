7a2ede33b119f509c0a527dac8f78cf9ab005a7e
Luc Billaud <billuc@hotmail.fr>
2025-09-30 11:50:18 +0200

    fix: showing hash check disabled only when necessary

--- test_libs/my_app/priv/migs/20250913102045-remove_lib.sql
+++ test_libs/my_app/priv/migs/20250913102045-remove_lib.sql
@@ -9,9 +9,9 @@ drop table books;
 --- 20250913102033-lib
 --- 20250913083538-Test1
 create table if not exists books (
-  id: serial primary key,
-  title: text,
-  rating: int,
+    id serial primary key,
+    title text,
+    rating int
 );
 --- 20250913091039-Test2
 alter table books
